4363a1ff0310546d069d5fbeff1505f83737ac73
shreytheshreyas <59922045+shreytheshreyas@users.noreply.github.com>
2022-03-17 22:30:14 +0800

    changes made to group by logic

--- src/simpledb/parse/Parser.java
+++ src/simpledb/parse/Parser.java
@@ -130,7 +130,7 @@ public class Parser {
          if(lex.matchKeyword("by")) {
             lex.eatKeyword("by");
             groupByFields = groupFieldList();
-            fields.addAll(groupByFields); //original implementation
+//            fields.addAll(groupByFields); //original implementation
 //            for(String groupfield : groupByFields) {
 //               fields.add("countof" + groupfield);
 //            }
@@ -201,7 +201,7 @@ public class Parser {
             lex.eatDelim('(');
             String fieldName = field();
             lex.eatDelim(')');
-            L.add(fieldName);
+            L.add("countof" + fieldName);
 
             switch(aggregateFunctionStr) {
                case "count": aggregateFunction = new CountFn(fieldName); break;
